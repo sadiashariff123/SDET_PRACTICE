5620336251e96fdec415c18401166de65b723808
sadiashariff123 <95458931+sadiashariff123@users.noreply.github.com>
2021-12-09 13:54:37 -0500

    Add files via upload
    Input from User using Scanner

---
+++ sadia.java
@@ -0,0 +1,20 @@
+package com.test;
+
+import java.util.Scanner;
+public class sadia {
+	
+	
+	public static void main(String[] args) {
+
+			Scanner scanner = new Scanner(System.in);
+			
+			System.out.println("What is your name?");
+			
+			String name = scanner.nextLine();
+			
+			System.out.println("hello" +name);
+	
+		
+	}
+
+}
