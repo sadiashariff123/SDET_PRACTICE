97e8412f0083338d1a3443cd0d0de5ce522c8049
sadiashariff123 <95458931+sadiashariff123@users.noreply.github.com>
2021-12-10 14:58:29 -0500

    Add files via upload
    Worked on Strings

---
+++ Dec2.java
@@ -0,0 +1,20 @@
+
+public class Dec2 {
+
+	public static void main(String[] args) {
+		System.out.println("DEC2");
+		
+		System.out.println("My daugther _ accidentlly stepped over _ new _");
+		
+		String adjective = "laila";
+		String Verb = "her";
+		String noun = "toy";
+		
+		
+		System.out.println("My daugther "+ adjective +" accidentlly stepped over "+ Verb +" new "+noun+"");
+		
+		
+		
+	}
+
+}
