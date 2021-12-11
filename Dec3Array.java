51719e624b1c65ef9335f4d90ab67382cdb2de44
sadiashariff123 <95458931+sadiashariff123@users.noreply.github.com>
2021-12-10 19:19:31 -0500

    Add files via upload
    Arrays[]

---
+++ Dec3Array.java
@@ -0,0 +1,18 @@
+
+public class Dec3Array {
+
+	public static void main(String[] args) {
+		
+		String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+		System.out.println (cars[0]);
+		
+		
+		System.out.println(cars.length);
+		
+		for (int i = 0; i < cars.length; i++) {
+			  System.out.println(cars[i]);
+
+	}
+
+}
+}
