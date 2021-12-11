9cccbb687e812ef72be57659b362c2b682ce9497
sadiashariff123 <95458931+sadiashariff123@users.noreply.github.com>
2021-12-10 20:26:05 -0500

    Add files via upload
    ForLoops{ }

---
+++ Dec4For.java
@@ -0,0 +1,17 @@
+
+public class Dec4For {
+
+	public static void main(String[] args) {
+		//for (statement 1; statement 2; statement 3) 
+		
+		for (int i = 0; i <= 10; i = i + 2) {
+			  System.out.println(i);
+		
+		}
+		String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+		for (String i : cars) {
+		  System.out.println(i);
+		}
+	}
+
+}
