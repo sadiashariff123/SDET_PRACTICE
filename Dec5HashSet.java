202b3e74ab75caa8a7061dac80deb9f7eba0a015
sadiashariff123 <95458931+sadiashariff123@users.noreply.github.com>
2021-12-10 23:48:21 -0500

    Add files via upload
    HashSet

---
+++ Dec5HashSet.java
@@ -0,0 +1,28 @@
+import java.util.HashSet;
+
+public class Dec5HashSet {
+
+	public static void main(String[] args) {
+		//import java.util.HashSet; 
+		// Import the HashSet class
+  
+		HashSet<String> h = new HashSet<String>();
+	    h.add("monkey");
+	    h.add("dog");
+	    h.add("hen");
+	    h.add("cat");
+	    h.add("rat");
+	    System.out.println(h);
+	  }
+	
+	}
+
+
+//h.contains("dog");
+//checks if it contains item
+//h.remove("dog");
+//removes an item
+//h.clear();
+//removes all list
+//// Create a HashSet object called numbers
+//HashSet<Integer> numbers = new HashSet<Integer>();
