7d819f87fb8fda0e626f1ff0bcef5bab7666dd9a
sadiashariff123 <95458931+sadiashariff123@users.noreply.github.com>
2021-12-10 19:20:34 -0500

    Add files via upload
    multidimensional Array

---
+++ Dec3multidimention.java
@@ -0,0 +1,23 @@
+
+public class Dec3multidimention {
+
+	public static void main(String[] args) {
+		int[][] numbers =  { {2,5,78},
+				  			{4,90,45},
+				  			{67,45,23}
+				  };
+		// {rows},{column}
+		//ForLoop
+		
+		
+	  for(int i=0; i<3; i++) {
+			for(int j=0; j<3; j++) {
+			System.out.println(numbers [i][j]);
+			}
+			
+		}
+		
+	}
+	
+}
+	
