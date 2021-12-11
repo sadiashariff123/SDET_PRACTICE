75326bd5a16058c881d225f040c306bb88f8f80c
sadiashariff123 <95458931+sadiashariff123@users.noreply.github.com>
2021-12-10 19:55:35 -0500

    Add files via upload
    While Loop (condition)

---
+++ Dec4Loops.java
@@ -0,0 +1,24 @@
+
+public class Dec4Loops {
+
+	public static void main(String[] args) {
+		//while loop
+		//while (condition)
+		
+		
+		int i = 0;
+		while (i < 5) {
+		  System.out.println(i);
+		  i++;
+		  
+		}
+
+	}
+	
+}
+
+
+//do {
+	  // code block to be executed
+//	}
+//	while (condition);
