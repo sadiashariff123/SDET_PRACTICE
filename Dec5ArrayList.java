03022cd651fc6490d5678ceaa016773515489c43
sadiashariff123 <95458931+sadiashariff123@users.noreply.github.com>
2021-12-10 22:18:16 -0500

    Add files via upload
    ArrayList

---
+++ Dec5ArrayList.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class Dec5ArrayList {
+
+	public static void main(String[] args) {
+		//import java.util.ArrayList; 
+		// import the ArrayList class
+		
+		
+			    ArrayList<String> cars = new ArrayList<String>();
+			    cars.add("Volvo");
+			    cars.add("BMW");
+			    cars.add("Ford");
+			    cars.add("Mazda");
+			   
+			    for (String i : cars) {
+			      System.out.println(i);
+			    }
+			  }
+
+	}
+//cars.size();
+//how many elements
+//cars.clear
+//for remove all the elements
+//cars.remove(0);
+//for removing an element
+
+
