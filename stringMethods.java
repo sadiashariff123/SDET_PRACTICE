cbb521631211c5ff154f150f8b29b924e54526c6
sadiashariff123 <95458931+sadiashariff123@users.noreply.github.com>
2021-12-13 22:32:51 -0500

    Add files via upload
    String Methods

---
+++ stringMethods.java
@@ -0,0 +1,16 @@
+
+public class stringMethods {
+
+	public static void main(String[] args) {
+		
+		String name ="Sandra Bob Rye" ;
+		
+		System.out.println("Name: "+ name);
+		System.out.println("Uppercase: " + name.toUpperCase());
+		System.out.println("Lowercase :" + name.toLowerCase());
+		System.out.println ("First Char :" + name.charAt(0));
+		System.out.println ("last Char :" + name.charAt(13));
+		System.out.println ("string :" + name.substring(10,14));
+	}
+
+}
