63a7112eef447fe839e36eed2d4813212bf2383a
sadiashariff123 <95458931+sadiashariff123@users.noreply.github.com>
2021-12-13 22:36:54 -0500

    Add files via upload
    String Builder

---
+++ Dec7StringBuilder.java
@@ -0,0 +1,12 @@
+
+public class Dec7StringBuilder {
+
+	public static void main(String[] args)
+	{
+		 StringBuilder sBuilder = new StringBuilder("Sulaiman is cute");
+		 sBuilder.append(" Cutest!");
+		 System.out.println(sBuilder);
+		}
+
+	}
+
