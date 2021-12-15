7337ca483f0d52b91d6204fa5be97a6c106183df
sadiashariff123 <95458931+sadiashariff123@users.noreply.github.com>
2021-12-14 22:33:52 -0500

    Add files via upload
    AbstractClass

---
+++ Dec10AbstractClass.java
@@ -0,0 +1,28 @@
+
+
+public class Dec10AbstractClass {
+
+	// Abstract class
+	abstract class Animal {
+	  // Abstract method (does not have a body)
+	  public abstract void animalSound();
+	  // Regular method
+	  public void sleep() {
+	    System.out.println("Zzz");
+	  }
+	}
+
+	// Subclass (inherit from Animal)
+	class Pig extends Animal {
+	  public void animalSound() {
+	    // The body of animalSound() is provided here
+	    System.out.println("The pig says: wee wee");
+	  }
+	}
+}
+	
+	
+
+	
+	
+
