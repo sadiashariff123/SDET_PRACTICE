f8c456070355c2b6fba2f375e6793ccf96f99d9b
sadiashariff123 <95458931+sadiashariff123@users.noreply.github.com>
2021-12-21 17:27:36 -0500

    Add files via upload
    Dec14-Selenium "Get Method"

---
+++ selenium.java
@@ -0,0 +1,15 @@
+package javatry;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class selenium {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\sadia\\eclipse-workspace\\SeleniumTryOut\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.udemy.com/join/login-popup/?skip_suggest=1&locale=en_US&next=https%3A%2F%2Fwww.udemy.com%2F&response_type=html");
+	}
+
+}
