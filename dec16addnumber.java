a51468379ba8ff383910d34d398aaa813f1c1dfd
sadiashariff123 <95458931+sadiashariff123@users.noreply.github.com>
2021-12-22 23:21:30 -0500

    Add files via upload
    Add-two-numbers

---
+++ dec16addnumber.java
@@ -0,0 +1,21 @@
+package javatry;
+
+import java.util.Scanner;
+
+public class dec16addnumber {
+
+	public static void main(String[] args) {
+		int x, y, sum;
+	    Scanner myObj = new Scanner(System.in); // Create a Scanner object
+	    System.out.println("Type a number:");
+	    x = myObj.nextInt(); // Read user input
+
+	    System.out.println("Type another number:");
+	    y = myObj.nextInt(); // Read user input
+
+	    sum = x + y;  // Calculate the sum of x + y
+	    System.out.println("Sum is: " + sum); // Print the sum
+
+	}
+
+}
